5dedb5b3c4d34b9a3745c297c10581613ad111a9
damianfral <huevofritopamojarpan@gmail.com>
2020-06-19 09:10:07 +0200

    Refactor shell.nix .

--- shell.nix
+++ shell.nix
@@ -1,1 +1,39 @@
-(import ./nix {}).myHaskellPackages.cloudinary-cli.env
+# let
+#   project = (import ./.).haskellPackages.server;
+# in
+#   pkgs.mkShell {
+#     buildInputs = project.env.nativeBuildInputs ++ [
+#       # pkgs.haskellPackages.cabal-install
+#       # pkgs.haskellPackages.cabal-helper
+#       # pkgs.haskellPackages.ghcide
+#       # pkgs.haskellPackages.apply-refact
+#       # pkgs.haskellPackages.hasktags
+#       # pkgs.haskellPackages.hindent
+#       pkgs.haskellPackages.hlint
+#       pkgs.haskellPackages.hoogle
+#       pkgs.haskellPackages.brittany
+#     ];
+# }
+
+
+# { pkgs }:
+# with pkgs;
+# mkShell {
+#   buildInputs = [
+#     haskellPackages.hlint
+#     haskellPackages.hoogle
+#     haskellPackages.brittany
+#   ];
+# }A
+
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    nixpkgs-fmt
+  ];
+
+  shellHook = ''
+    # ...
+  '';
+}
