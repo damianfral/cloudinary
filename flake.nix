11599593ee5cecada3fbe62f0231a53aaa206137
damianfral <huevofritopamojarpan@gmail.com>
2021-04-30 05:12:16 +0200

    Use nixpkgs master and format nix code.

--- flake.nix
+++ flake.nix
@@ -2,35 +2,41 @@
   description = "Cloudinary client API";
 
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-  inputs.utils.url   = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, utils }@inputs:
 
     let
       system = "x86_64-linux";
-      pkgs   = import nixpkgs
-      { inherit system;
-        overlays = [ self.overlay ];
-      };
+      pkgs = import nixpkgs
+        {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
 
     in
-      {
-        overlay = import ./overlay;
+    {
+      overlay = import ./overlay;
 
-        packages.${system} =
-          with pkgs.haskellPackages;
-          { inherit cloudinary; };
+      packages.${system} =
+        with pkgs.haskellPackages;
+        { inherit cloudinary; };
 
-        devShell.${system}       = self.packages.${system}.cloudinary.env;
-        defaultPackage.${system} = self.packages.${system}.cloudinary;
-        apps.${system} =
-          { cloudinary-cli = {
-              type = "app";
-              program = "${pkgs.haskellPackages.cloudinary}/bin/cloudinary-cli";
-            };
-          };
-
-        defaultApp.${system} = self.apps.${system}.cloudinary-cli;
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [ cabal-install ghcid ormolu nixpkgs-fmt ]
+          ++ self.packages.${system}.cloudinary.env.nativeBuildInputs;
       };
+      defaultPackage.${system} = self.packages.${system}.cloudinary;
+      apps.${system} =
+        {
+          cloudinary-cli = {
+            type = "app";
+            program = "${pkgs.haskellPackages.cloudinary}/bin/cloudinary-cli";
+          };
+        };
+
+      defaultApp.${system} = self.apps.${system}.cloudinary-cli;
+    };
 }
