a375e6d6efdb2a82088d8cae73aa365e9dcf6059
damianfral <huevofritopamojarpan@gmail.com>
2025-04-07 16:07:55 +0000

    Format.

--- flake.nix
+++ flake.nix
@@ -4,41 +4,39 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/24.11";
   inputs.utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, utils }@inputs:
-
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs
-        {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-
-    in
-    {
-      overlay = import ./overlay;
-
-      packages.${system} =
-        with pkgs.haskellPackages;
-        { inherit cloudinary; };
-
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [ cabal-install ghcid ormolu nixpkgs-fmt ]
-          ++ self.packages.${system}.cloudinary.env.nativeBuildInputs;
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs =
+      import nixpkgs
+      {
+        inherit system;
+        overlays = [self.overlay];
       };
-      defaultPackage.${system} = self.packages.${system}.cloudinary;
-      apps.${system} =
-        {
-          cloudinary-cli = {
-            type = "app";
-            program = "${pkgs.haskellPackages.cloudinary}/bin/cloudinary-cli";
-          };
-        };
+  in {
+    overlay = import ./overlay;
 
-      defaultApp.${system} = self.apps.${system}.cloudinary-cli;
-      checks.${system} = {
-        build = self.defaultPackage.${system};
+    packages.${system} = with pkgs.haskellPackages; {inherit cloudinary;};
+
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs;
+        [cabal-install ghcid ormolu nixpkgs-fmt]
+        ++ self.packages.${system}.cloudinary.env.nativeBuildInputs;
+    };
+    defaultPackage.${system} = self.packages.${system}.cloudinary;
+    apps.${system} = {
+      cloudinary-cli = {
+        type = "app";
+        program = "${pkgs.haskellPackages.cloudinary}/bin/cloudinary-cli";
       };
     };
+
+    defaultApp.${system} = self.apps.${system}.cloudinary-cli;
+    checks.${system} = {
+      build = self.defaultPackage.${system};
+    };
+  };
 }
